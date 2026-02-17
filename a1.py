464afa616a8ee3c8789ad53820fa651f03733695
Ayesha733 <h.ayeshasaddiqua@gmail.com>
2026-02-17 08:26:14 -0700

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,6 @@
+n1=[1,2,3,4,5]
+n2=[6,7,8,9,10]
+print("n1=",n1)
+print("n2=",n2)
+sum=map(lambda x,y:x+y,n1,n2)
+print("s=",list(sum))
---
+++ a3.py
@@ -0,0 +1,5 @@
+student=["Ayesha","Amna","Ayat","Anam"]
+roll=[1,2,3,4]
+
+dict={s:r for s,r in zip(student,roll)}
+print(dict)
